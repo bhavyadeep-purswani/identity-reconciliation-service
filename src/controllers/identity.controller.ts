dafb24aaca5c70bfa0a17abe720f3a65c79dcaa4
Bhavyadeep <bhavyadeep.purswani98@gmail.com>
2025-02-12 02:15:51 +0530

    Add prisma, integrate DB, create db schema

--- src/controllers/identity.controller.ts
+++ src/controllers/identity.controller.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from "express";
-export const identify = (req: Request, res: Response) => {
+import { prisma } from "../lib/prisma";
+export const identify = async (req: Request, res: Response) => {
     //TODO: Implement
     console.log(req.body);
     res.status(200).json({});
